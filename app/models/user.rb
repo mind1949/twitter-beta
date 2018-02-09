d9e6fd1d874030511cb70f4826b75acaf2cc0aa0
zhulianjie <nonzeroking@hotmail.com>
2018-02-09 22:49:22 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts,dependent: :destroy
+
   attr_accessor :remember_token,:activation_token,:reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -77,6 +79,11 @@ class User < ApplicationRecord
 
   def password_reset_expired?
     self.reset_sent_at < 2.hour.ago
+  end
+
+  def feed
+    #Micropost.where("user_id = ?", id)
+    microposts
   end
 
 
