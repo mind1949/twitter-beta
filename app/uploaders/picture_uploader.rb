214c272a7f7915618b291a208b650046019d7469
zhulianjie <nonzeroking@hotmail.com>
2018-02-10 20:29:13 +0800

    fix bug of uploading picture

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,7 +9,7 @@ class PictureUploader < CarrierWave::Uploader::Base
     storage :fog
   else
     storage :file
-  end  # storage :fog
+  end  
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
